3d14b53a6bccc4c19a7704f1e6695095ffb10661
Rémi Moustey <remimoustey@gmail.com>
2020-08-15 17:25:37 +0200

    Geolocation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { MapContainer } from './App/MapContainer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const App = () => (
--- src/App/MapContainer.jsx
+++ src/App/MapContainer.jsx
@@ -2,22 +2,43 @@ import React, { Component } from 'react';
 import GoogleMapReact  from 'google-map-react';
 
 export class MapContainer extends Component {
-  static defaultProps = {
-    center: {
-      lat: 59.95,
-      lng: 30.33
-    },
-    zoom: 11
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      center: null,
+      zoom: 7
+    }
+  }
 
-  render() {
+  componentDidMount = () => {
+    if("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        setTimeout(() => {
+          this.setState({
+            center: {
+              lat: pos.coords.latitude,
+              lng: pos.coords.longitude
+            }
+          })
+        }, 1000);
+      });
+    } else {
+      console.error("Erreur : impossible de détemriner votre position.");
+    }
+  }
+
+  render = () => {
+    if(this.state.center === null) {
+      return (
+        <p className="ml-2">Chargement en cours...</p>
+      );
+    }
     return (
-      // Important! Always set the container height explicitly
       <div id="map" style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyC7gHF4noGLTnBRfnHvQqmKU6GtK6D-ggM' }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
+          defaultCenter={this.state.center}
+          defaultZoom={this.state.zoom}
         />
       </div>
     );
